f6c0fec7ecfec4475e290ab98c0b6e58eb86c14b
Anirban Basak <jeetbasak2002@gmail.com>
2023-05-28 17:38:50 +0530

    feat - Line Chart Initial prototype

--- app/src/main/java/com/dev/anirban/chartlibrary/MainActivity.kt
+++ app/src/main/java/com/dev/anirban/chartlibrary/MainActivity.kt
@@ -3,13 +3,22 @@ package com.dev.anirban.chartlibrary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
+import com.dev.anirban.chartlibrary.charts.linechart.LineChart
+import com.dev.anirban.chartlibrary.charts.linechart.LineChartData
+import com.dev.anirban.chartlibrary.charts.linechart.LineChartDecoration
 import com.dev.anirban.chartlibrary.ui.theme.ChartLibraryTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +31,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    Column {
+                        ElevatedCard(
+                            modifier = Modifier
+                                .height(200.dp)
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            elevation = CardDefaults.cardElevation(8.dp),
+                        ) {
+
+                            val lineChartData = LineChartData(
+                                yAxisReadings = listOf(listOf(6f, 5f, 4f, 6f, 7.5f, 7f, 6f)),
+                                xAxisReadings = listOf(
+                                    "Jan",
+                                    "Mar",
+                                    "May",
+                                    "Jul",
+                                    "Sep",
+                                    "Nov",
+                                    "Dec"
+                                ),
+                                numOfXMarkers = 7,
+                                numOfYMarkers = 5
+                            )
+
+                            val lineChartDecoration = LineChartDecoration(
+                                textColor = MaterialTheme.colorScheme.onSurface.toArgb(),
+                                lineColor = listOf(Color.Blue),
+                                dotColor = listOf(Color.Green)
+                            )
+
+                            LineChart(
+                                lineChartData,
+                                lineChartDecoration
+                            ).BuildGraph()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChartLibraryTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/dev/anirban/chartlibrary/charts/linechart/LineChartDecoration.kt
@@ -0,0 +1,16 @@
+package com.dev.anirban.chartlibrary.charts.linechart
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * This data class contains all the decoration data  related to the Line Chart
+ *
+ * @param textColor This is the text color to be used in the line chart
+ * @param lineColor This is the line color to be used in the Line Chart
+ * @param dotColor THis is the dot color to be used in the Line Chart
+ */
+data class LineChartDecoration(
+    val textColor: Int,
+    val lineColor: List<Color>,
+    val dotColor: List<Color>
+)
